da7dc9e36fcc99bcb6b6f29a2545d7a729cd0598
Rakesh Chhabria <chhabriarakesh1511@gmail.com>
2023-10-13 11:32:31 +0530

    Pushing Fuse Code

---
+++ workspace/restCreateCustomer/target/generated/src/main/java/org/example/createcustomer_wsdl/CreateCustomerRequest.java
@@ -0,0 +1,176 @@
+
+package org.example.createcustomer_wsdl;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="CustID" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="FirstName" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="LastName" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="City" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="Pincode" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "custID",
+    "firstName",
+    "lastName",
+    "city",
+    "pincode"
+})
+@XmlRootElement(name = "CreateCustomerRequest")
+public class CreateCustomerRequest {
+
+    @XmlElement(name = "CustID", required = true)
+    protected String custID;
+    @XmlElement(name = "FirstName", required = true)
+    protected String firstName;
+    @XmlElement(name = "LastName", required = true)
+    protected String lastName;
+    @XmlElement(name = "City", required = true)
+    protected String city;
+    @XmlElement(name = "Pincode", required = true)
+    protected String pincode;
+
+    /**
+     * Gets the value of the custID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCustID() {
+        return custID;
+    }
+
+    /**
+     * Sets the value of the custID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCustID(String value) {
+        this.custID = value;
+    }
+
+    /**
+     * Gets the value of the firstName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the value of the firstName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFirstName(String value) {
+        this.firstName = value;
+    }
+
+    /**
+     * Gets the value of the lastName property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Sets the value of the lastName property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    /**
+     * Gets the value of the city property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Sets the value of the city property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCity(String value) {
+        this.city = value;
+    }
+
+    /**
+     * Gets the value of the pincode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPincode() {
+        return pincode;
+    }
+
+    /**
+     * Sets the value of the pincode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPincode(String value) {
+        this.pincode = value;
+    }
+
+}
---
+++ workspace/restCreateCustomer/target/generated/src/main/java/org/example/createcustomer_wsdl/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/CreateCustomer_WSDL/")
+package org.example.createcustomer_wsdl;
